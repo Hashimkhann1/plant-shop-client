60f572c592a7729da540595193abb4110840a44e
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-30 23:14:35 +0500

    new color is added

--- lib/utils/app_colors/app_colors.dart
+++ lib/utils/app_colors/app_colors.dart
@@ -7,5 +7,6 @@ class AppColors {
 
   static const Color primaryColor = Color(0xFF40744d);
   static const Color whiteColor = Color(0xFFffffff);
+  static const Color blackColor = Color(0xFF000000);
 
 }
