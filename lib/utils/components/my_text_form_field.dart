7a4a469041641f9ecb669f8ec825ceae2e4728f3
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-07-02 17:34:42 +0500

    my text form field

---
+++ lib/utils/components/my_text_form_field.dart
@@ -0,0 +1,63 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:plant_cli/utils/app_colors/app_colors.dart';
+
+class MyTextFormFeild extends StatelessWidget {
+  final String? hintText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final int? maxLines;
+  final bool obscureText;
+  final Color? fillColor;
+  final BorderSide borderSide;
+  final Color? focusedBorderColor;
+  final double borderRadius;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+
+  const MyTextFormFeild(
+      {super.key,
+      required this.hintText,
+        this.prefixIcon,
+      this.suffixIcon,
+        this.maxLines = 1,
+      this.obscureText = false,
+      this.fillColor = Colors.white,
+      this.borderSide = const BorderSide(
+        color: Colors.black,
+      ),
+        this.focusedBorderColor = AppColors.primaryColor,
+      this.controller,
+      this.borderRadius = 10,
+      this.validator});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscureText,
+      controller: controller,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          hintText: hintText,
+          filled: true,
+          fillColor: fillColor,
+          border: OutlineInputBorder(
+              borderSide: borderSide,
+              borderRadius: BorderRadius.circular(borderRadius)
+          ),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: focusedBorderColor!),
+            borderRadius: BorderRadius.circular(borderRadius)
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderSide: borderSide,
+            borderRadius: BorderRadius.circular(borderRadius)
+        ),
+      ),
+      validator: validator,
+    );
+  }
+}
