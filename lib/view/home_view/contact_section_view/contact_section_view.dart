c5fc30f08f01be66e755fc651b2cefa3f01e660e
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-07-05 23:04:04 +0500

    ui and mail sended completed

--- lib/view/home_view/contact_section_view/contact_section_view.dart
+++ lib/view/home_view/contact_section_view/contact_section_view.dart
@@ -1,16 +1,27 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plant_cli/utils/app_colors/app_colors.dart';
 import 'package:plant_cli/utils/components/my_text.dart';
 import 'package:plant_cli/utils/components/my_text_button.dart';
 import 'package:plant_cli/utils/components/my_text_form_field.dart';
 import 'package:plant_cli/utils/responsive/responsive.dart';
+import 'package:plant_cli/view_model/bloc/loading_bloc/loading_bloc/loading_bloc.dart';
+import 'package:plant_cli/view_model/bloc/loading_bloc/loading_bloc_event/loading_bloc_event.dart';
+import 'package:plant_cli/view_model/bloc/loading_bloc/loading_bloc_state/loading_bloc_state.dart';
+import 'package:plant_cli/view_model/cntact_us_view_model/contact_us_view_model.dart';
 
 class ContactSectionView extends StatelessWidget {
-  const ContactSectionView({
+  ContactSectionView({
     super.key,
   });
+
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +37,7 @@ class ContactSectionView extends StatelessWidget {
           SizedBox(
             height: height * 0.02,
           ),
-          MyText(
+          const MyText(
             title: "Contact",
             fontSize: 30,
             fontWeight: FontWeight.bold,
@@ -35,38 +46,76 @@ class ContactSectionView extends StatelessWidget {
           SizedBox(
             height: height * 0.02,
           ),
-          MyTextFormFeild(
-            hintText: "Name",
-            focusedBorderColor: AppColors.primaryColor,
+          Form(
+            key: _formKey,
+              child: Column(
+                children: [
+                  MyTextFormFeild(
+                    controller: nameController,
+                    hintText: "Name",
+                    focusedBorderColor: AppColors.primaryColor,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  MyTextFormFeild(
+                    controller: emailController,
+                    hintText: "your Gmail",
+                    focusedBorderColor: AppColors.primaryColor,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Enter your gmail";
+                      }else if(!value.contains("@gmail.com")){
+                        return "Enter valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  MyTextFormFeild(
+                    controller: messageController,
+                    hintText: "Message",
+                    maxLines: 4,
+                    focusedBorderColor: AppColors.primaryColor,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Enter your your message";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                ],
+              )
           ),
-          SizedBox(
-            height: height * 0.02,
-          ),
-          MyTextFormFeild(
-            hintText: "Email",
-            focusedBorderColor: AppColors.primaryColor,
-          ),
-          SizedBox(
-            height: height * 0.02,
-          ),
-          MyTextFormFeild(
-            hintText: "Message",
-            maxLines: 4,
-            focusedBorderColor: AppColors.primaryColor,
-          ),
-          SizedBox(
-            height: height * 0.02,
-          ),
-          MyTextButton(
-            title: 'Send',
-            textColor: AppColors.whiteColor,
-            backgroundColor: AppColors.primaryColor,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            height: height * 0.06,
-            width: Responsive.isDesktop(context) ?  width * 0.12 : width * 0.28,
-            borderRadius: 8,
-          ),
+          BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state) {
+            return MyTextButton(
+              title: 'Send',
+              textColor: AppColors.whiteColor,
+              backgroundColor: AppColors.primaryColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              height: height * 0.06,
+              width: Responsive.isDesktop(context) ?  width * 0.12 : width * 0.28,
+              borderRadius: 8,
+              isLoading: state.isLoading,
+              onTap: () {
+                if(_formKey.currentState!.validate()){
+                  ContactUsViewModel().sendEmail(context, nameController.text.toString(), emailController.text.toString(), messageController.text.toString());
+                }
+              },
+            );
+          },),
 
           SizedBox(
             height: height * 0.02,
