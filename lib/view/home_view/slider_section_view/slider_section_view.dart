5ef80966d082e33d37bffa91facfe3677d88b4a0
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-07-09 16:26:10 +0500

    new images is added

--- lib/view/home_view/slider_section_view/slider_section_view.dart
+++ lib/view/home_view/slider_section_view/slider_section_view.dart
@@ -2,6 +2,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_cli/utils/responsive/responsive.dart';
+import 'package:plant_cli/utils/view.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SliderSectionView extends StatefulWidget {
@@ -15,6 +16,10 @@ class _SliderSectionViewState extends State<SliderSectionView> {
   int activeIndex = 0;
 
   final List<String> imagesUrl = [
+    // 'https://raw.githubusercontent.com/Hashimkhann1/plant_shop/main/images/3.png',
+    // 'https://raw.githubusercontent.com/Hashimkhann1/plant_shop/main/images/1.png',
+    // 'https://raw.githubusercontent.com/Hashimkhann1/plant_shop/main/images/4.png',
+    // 'https://raw.githubusercontent.com/Hashimkhann1/plant_shop/main/images/2.png',
     '3',
     '1',
     '4',
@@ -40,12 +45,13 @@ class _SliderSectionViewState extends State<SliderSectionView> {
                   borderRadius: BorderRadius.circular(6)),
               child: Image.asset(
                 'images/slider/${imagesUrl[index]}.png',
+                // imagesUrl[index].toString(),
                 fit: BoxFit.cover,
               ),
             );
           },
           options: CarouselOptions(
-              height: Responsive.isMobile(context) ? height * 0.23 : height * 0.40,
+              height: Responsive.isMobile(context) ? height * 0.23 : Responsive.isTablet(context) ? height * 0.28 : height * 0.40,
               autoPlay: true,
               autoPlayInterval: const Duration(seconds: 4),
               viewportFraction: 0.8,
@@ -64,8 +70,8 @@ class _SliderSectionViewState extends State<SliderSectionView> {
           activeIndex: activeIndex,
           count: imagesUrl.length,
           duration: const Duration(milliseconds: 500),
-          effect: const WormEffect(
-              dotColor: Colors.white, activeDotColor: Colors.greenAccent),
+          effect: WormEffect(
+              dotColor: Colors.white, activeDotColor: Colors.greenAccent,dotHeight: Responsive.isMobile(context) ? 12 : 16,dotWidth: Responsive.isMobile(context) ? 12 : 16,),
         ),
       )
     ]);
